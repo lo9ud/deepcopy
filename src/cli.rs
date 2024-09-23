2e2a67de7ebddc5d36392af395d94e6f0ac2a544
Adam Kent <adam.dkent@icloud.com>
2024-09-23 16:22:14 +0100

    Update help info

--- src/cli.rs
+++ src/cli.rs
@@ -6,7 +6,7 @@ use crate::conflict_resolver::ConflictResolutionStrategy;
 
 
 #[derive(Parser, Debug)]
-#[command(about = "A deep copying utility")]
+#[command(about = env!("CARGO_PKG_DESCRIPTION"), version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), long_about = "This tool is designed to be idempotent, such that it can be interrrupted repeatedly and rerun without damage to files or data.")]
 pub struct Cli {
     #[arg(short, long, value_hint= ValueHint::FilePath, value_parser = value_parser!(PathBuf))]
     pub source: PathBuf,
